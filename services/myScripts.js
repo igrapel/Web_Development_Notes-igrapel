e337efd0e6e088374dddf4982ca85babf4dd4025
Ilan Grapel <igrapel@users.noreply.github.com>
2020-01-22 11:15:52 -0500

    Update myScripts.js

--- services/myScripts.js
+++ services/myScripts.js
@@ -1,17 +1,10 @@
 
 
-
-
-function myFunction() {
-
+function myLoop(){ 
+var end = document.getElementById("end").value;
 var start = document.getElementById("start").value;
-var end= document.getElementById("end").value;
-//var jump = document.getElementById("jump").value;
-  console.log(typeof jump);
-  for (var i = start; i <= end; i++) {
-    console.log(typeof i);
-document.write( '<p>' + i + '</p>');
-   
-   console.log(i);
-}
-}
+for(var i = start; i <= end; i++)
+  {
+    document.write('<p>' + i);
+  }
+  }
