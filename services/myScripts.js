17801a82a7958e1d49a161ce0928c83edd52757c
Ilan Grapel <igrapel@users.noreply.github.com>
2020-01-22 10:30:08 -0500

    Update myScripts.js

--- services/myScripts.js
+++ services/myScripts.js
@@ -6,7 +6,7 @@ function myFunction() {
 
 var start = document.getElementById("start").value;
 var end= document.getElementById("end").value;
-var jump = document.getElementById("jump").value;
+//var jump = document.getElementById("jump").value;
   console.log(typeof jump);
   for (var i = start; i <= end; i++) {
     console.log(typeof i);
