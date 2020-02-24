fa0d5c1c853a3cf0b7ab0de12677aaaa82c52c37
Ilan Grapel <igrapel@users.noreply.github.com>
2020-02-23 21:09:35 -0500

    Update myScripts.js

--- services/myScripts.js
+++ services/myScripts.js
@@ -1,12 +1,10 @@
 var show = document.getElementById('show'); 
-        
-          
-        function Factorial(n) { 
+function Factorial(n) { 
          
-            var ans=1; 
+var ans=1; 
               
-            for (var i = 2; i <= n; i++) 
-                ans = ans * i; 
+ for (var i = 2; i <= n; i++) 
+    ans = ans * i; 
             return ans; 
         } 
           
