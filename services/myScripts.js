657051c0e9db27b4f90dfe8b2686d63c53f8a9ab
Ilan Grapel <igrapel@users.noreply.github.com>
2020-03-09 12:42:46 -0400

    Update myScripts.js

--- services/myScripts.js
+++ services/myScripts.js
@@ -1,45 +1,23 @@
-  function fillTestResults() {
-            var testResults = document.getElementById('testResults');
-            var firstName = document.getElementById('firstName').value;
-            var lastName = document.getElementById('lastName').value;
-            var found = document.getElementById('found').value;
-            testResults.innerHTML = "You entered " + firstName + " " + lastName
-                + " for a name. You found us via " + found + ".";
-        }
+ function fillState(){
+var myState = document.getElementById('state');
+var mydropdown = document.getElementById('location');
+if(mydropdown.value == "Miami")
+{
+     myState.value = "FL";
+}
 
- function fillState(location) {
-            var state = document.getElementById('state');
-            if (state.value == '') {
-                if (location == 'Miami') {
-                    state.value = 'FL';
-                }
-                else if (location == 'New York City') {
-                    state.value = 'NY';
-                }
-                else {
-                    state.value = '';
-                }
-            }
-        }
+else if(mydropdown.value == "NYC")
+{
+    myState.value = "NY";
+}
 
-
+else
+{
+mytextbox.value = "";
+}
+}
+        
+        
         function convertUpper(text, field) {
             document.getElementById(field).value = text.toUpperCase();
-        }
-
-        function convertLower(text, field) {
-            document.getElementById(field).value = text.toLowerCase();
-        }
-        function checkCity(city) {
-            city = city.toUpperCase();
-            if (city == 'New York City' || city == 'Atlanta' || city == 'Madrid') {
-                document.getElementById('testResults').innerHTML = "Your chosen location will only be available during the summer";
-            }
-
-        }
-        function checkFields() {
-            if (document.getElementById('firstName').value == '' ||// add operator
-                document.getElementById('lastName').value == '') {
-                alert('One or more required name fields is empty');
-            }
         }
