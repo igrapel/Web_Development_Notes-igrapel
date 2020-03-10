d28c0cfc1d7fa69400fac1f0e43cc9784a18a138
Ilan Grapel <igrapel@users.noreply.github.com>
2020-03-09 21:57:03 -0400

    Update myScripts.js

--- services/myScripts.js
+++ services/myScripts.js
@@ -1,23 +1,31 @@
- function fillState(){
-var myState = document.getElementById('state');
-var mydropdown = document.getElementById('location');
-if(mydropdown.value == "Miami")
+function fillState()
 {
-     myState.value = "FL";
+    var myState = document.getElementById('state');
+    var mydropdown = document.getElementById('location');
+    if(mydropdown.value == "Miami")
+    {
+        myState.value = "FL";
+    }
+
+    else if(mydropdown.value == "NYC")
+    {
+        myState.value = "NY";
+    }
+    else
+    {
+        mytextbox.value = "";
+    }
 }
 
-else if(mydropdown.value == "NYC")
+function convertUpper(text, field) 
 {
-    myState.value = "NY";
+    document.getElementById(field).value = text.toUpperCase();
 }
 
-else
+function checkField()
 {
-mytextbox.value = "";
+  if(document.getElementById("firstName").value=="" || document.getElementById("lastName").value=="")
+    {
+      alert("You must give your first name.")
+    }
 }
-}
-        
-        
-        function convertUpper(text, field) {
-            document.getElementById(field).value = text.toUpperCase();
-        }
